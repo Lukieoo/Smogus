acc310dc0e7e48e8436e2773bd062bf3ee205d9a
lukieoo <pawkrzysciak@gmail.com>
2020-03-29 23:10:16 +0200

    add Spinner

--- app/src/main/java/com/anioncode/smogu/Activity/MainActivity.kt
+++ app/src/main/java/com/anioncode/smogu/Activity/MainActivity.kt
@@ -48,6 +48,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setPermission()
 
         val nameSensor = arrayOf(
+            "Wybierz",
             "ST",
             "PM10",
             "PM2.5",
@@ -62,7 +63,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
 
         spinner.adapter = SpinnerAdapter(applicationContext, nameSensor)
-
+        spinner.setSelection(0, false);
         spinner?.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
 
             override fun onItemSelected(
@@ -71,11 +72,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 position: Int,
                 id: Long
             ) {
-                stationList = emptyList()
-                sizedApplication = 0
-                modelIndexList.clear()
 
                 SELECTED = nameSensor.get(position);
+
+                var frg: Fragment? = null
+                frg = supportFragmentManager.findFragmentByTag("SOMETAG")
+                val ft: FragmentTransaction =
+                    supportFragmentManager.beginTransaction()
+                frg?.let { ft.detach(it) }
+                frg?.let { ft.attach(it) }
+                ft.commit()
+                sync.pauseAnimation()
 
             } // to close the onItemSelected
 
@@ -164,7 +171,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
 
             override fun onAnimationStart(p0: Animator?) {
-
+                spinner.setSelection(0)
                 val service =
                     RetrofitClientInstance.getRetrofitInstance()!!.create(ApiService::class.java)
                 stationList = emptyList()
@@ -195,6 +202,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                                         frg?.let { ft.attach(it) }
                                         ft.commit()
                                         sync.pauseAnimation()
+
                                     }
                                     println("moveIN $k ${modelIndexList.size}")
                                 }, {
@@ -239,31 +247,36 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 R.id.fragment,
                 StatsFragment(), "SOMETAG"
             ).commit()
-            spinner.visibility =View.GONE
+            spinner.visibility = View.GONE
+            spinner.setSelection(0)
         } else if (id == R.id.maps) {
             supportFragmentManager.beginTransaction().replace(
                 R.id.fragment,
                 MapFragment(), "SOMETAG"
             ).commit()
-            spinner.visibility =View.VISIBLE
+            spinner.visibility = View.VISIBLE
+            spinner.setSelection(0)
         } else if (id == R.id.info) {
             supportFragmentManager.beginTransaction().replace(
                 R.id.fragment,
                 InfoFragment(), "SOMETAG"
             ).commit()
-            spinner.visibility =View.GONE
+            spinner.visibility = View.GONE
+            spinner.setSelection(0)
         } else if (id == R.id.stats) {
             supportFragmentManager.beginTransaction().replace(
                 R.id.fragment,
                 ChartFragment(), "SOMETAG"
             ).commit()
-            spinner.visibility =View.GONE
+            spinner.visibility = View.GONE
+            spinner.setSelection(0)
         } else if (id == R.id.about) {
             supportFragmentManager.beginTransaction().replace(
                 R.id.fragment,
                 AboutFragment(), "SOMETAG"
             ).commit()
-            spinner.visibility =View.GONE
+            spinner.visibility = View.GONE
+            spinner.setSelection(0)
         }
 
         drawer.closeDrawer(GravityCompat.START)
